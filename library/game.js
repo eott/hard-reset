bbaebe58c360446aa36634e418878c26cace11e1
Etienne Ott <eti.ott@gmail.com>
2017-07-30 08:21:45 +0200

    Move input functions into own wrapper.

--- library/game.js
+++ library/game.js
@@ -1,42 +1,52 @@
 App = function() {
     this.can = document.getElementById("screen")
     this.ctx = this.can.getContext("2d")
-    this.keyStatus = [0, 0, 0, 0, 0]
 
-    this.registerListeners()
+    this.shiftX = 0
+    this.shiftY = 0
+
+    this.input = new Input(this)
+
+    this.debugPoints = []
+    for (var i = 0; i < 100; i++) {
+        this.debugPoints[i] = [Math.random() * 2000 - 1000, Math.random() * 2000 - 1000]
+    }
 
     this.loop = window.setInterval('app.gameLoop()', 50);
 }
 
 App.prototype.gameLoop = function() {
+    this.draw()
+}
 
+App.prototype.circle = function(x, y, radius, color, width, fill) {
+    this.ctx.beginPath()
+    this.ctx.arc(x, y, radius, 0, 2 * Math.PI, false)
+    this.ctx.lineWidth = width
+    this.ctx.strokeStyle = color
+    this.ctx.fillStyle = color
+    if (fill) {
+        this.ctx.fill()
+    } else {
+        this.ctx.stroke()
+    }
 }
 
 App.prototype.draw = function() {
     this.ctx.clearRect(0, 0, this.can.width, this.can.height)
 
+    for (var i = 0; i < 100; i++) {
+        // Draw aura
+        var x = this.shiftX + this.debugPoints[i][0]
+        var y = this.shiftY + this.debugPoints[i][1]
+        var grad = this.ctx.createRadialGradient(x, y, 0, x, y, 50)
+        grad.addColorStop(0, 'rgba(255,40,40,0.2)')
+        grad.addColorStop(1, 'rgba(255,40,40,0.0)')
+        this.circle(x, y, 50, grad, 1, true)
+        // Draw body
+        this.circle(x, y, 3, '#ff2828', 1, true)
+    }
+
     this.ctx.font = "17px Consolas"
     this.ctx.fillStyle = "#ffffff"
-}
-
-App.prototype.keyEvent = function(type, key) {
-    // w = 87, a = 65, s = 83, d = 68, space = 32
-    var index = [87, 65, 83, 68, 32].indexOf(key)
-    
-    if (index == -1) {
-        return
-    }
-
-    this.keyStatus[index] = type
-
-    if (this.keyStatus[index] == 0 && type == 1) {
-        this.keyStatus[index] = 1
-    } else if (this.keyStatus[index] > 0 && type == 0) {
-        this.keyStatus[index] = 0
-    }
-}
-
-App.prototype.registerListeners = function() {
-    window.addEventListener("keydown", function(e) {app.keyEvent(1, e.keyCode);}, false);
-    window.addEventListener("keyup", function(e) {app.keyEvent(0, e.keyCode);}, false);
 }
---
+++ library/input.js
@@ -0,0 +1,50 @@
+Input = function(app) {
+    this.app = app
+
+    this.keyStatus = [0, 0, 0, 0, 0]
+    
+    this.mouseStatus = 0
+    this.mouseDiff = [0, 0]
+    this.mousePos = [0, 0]
+
+    this.registerListeners()
+}
+
+Input.prototype.registerListeners = function() {
+    window.addEventListener("keydown", function(e) {this.keyEvent(1, e.keyCode);}, false)
+    window.addEventListener("keyup", function(e) {this.keyEvent(0, e.keyCode);}, false)
+}
+
+
+Input.prototype.mouseUpDown = function(ev) {
+    this.mouseStatus = ev.buttons
+}
+
+Input.prototype.mouseMove = function(ev) {
+    this.mouseDiff = [ev.layerX - this.mousePos[0], ev.layerY - this.mousePos[1]]
+    this.mousePos = [ev.layerX, ev.layerY]
+
+    if (this.mouseStatus == 1) {
+        this.app.shiftX += this.mouseDiff[0]
+        this.app.shiftY += this.mouseDiff[1]
+        this.app.shiftX = Math.min(1000, Math.max(-1000, this.app.shiftX))
+        this.app.shiftY = Math.min(1000, Math.max(-1000, this.app.shiftY))
+    }
+}
+
+Input.prototype.keyEvent = function(type, key) {
+    // w = 87, a = 65, s = 83, d = 68, space = 32
+    var index = [87, 65, 83, 68, 32].indexOf(key)
+    
+    if (index == -1) {
+        return
+    }
+
+    this.keyStatus[index] = type
+
+    if (this.keyStatus[index] == 0 && type == 1) {
+        this.keyStatus[index] = 1
+    } else if (this.keyStatus[index] > 0 && type == 0) {
+        this.keyStatus[index] = 0
+    }
+}
